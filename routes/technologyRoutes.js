36c363a24f2022a8de6e1d3142dd2fc2b513dd3d
[Ibukunoluwa Adekunle] <[ibkadekunle2004@gmail.com]>
2025-09-23 08:20:22 +0100

    technologies

--- routes/technologyRoutes.js
+++ routes/technologyRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getTechnologies, addTechnology, updateTechnology, deleteTechnology, getTechnology } from "../controllers/technologyController.js";
 import { upload } from "../config/cloudinary.js";
-import { verifyToken } from "../middleware/auth.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
@@ -10,8 +10,8 @@ router.get("/", getTechnologies);
 router.get("/:id", getTechnology);
 
 // Admin routes (protected)
-router.post("/", verifyToken, upload.single("icon"), addTechnology);
-router.put("/:id", verifyToken, upload.single("icon"), updateTechnology);
-router.delete("/:id", verifyToken, deleteTechnology);
+router.post("/", auth, upload.single("icon"), addTechnology);
+router.put("/:id", auth, upload.single("icon"), updateTechnology);
+router.delete("/:id", auth, deleteTechnology);
 
 export default router;
